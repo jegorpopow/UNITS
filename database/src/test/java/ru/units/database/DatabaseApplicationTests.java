1907a30b64c83541e16747e5209019b73d3f32cf
Jegor Popow <juicedogegor@gmail.com>
2023-03-06 13:55:46 +0300

    creating project

---
+++ database/src/test/java/ru/units/database/DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.units.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
