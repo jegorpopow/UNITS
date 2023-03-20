77287143ca3e6663a0af020e18d4062a0f8401c7
Lily <lilyreber@mail.ru>
2023-03-20 11:42:11 +0300

    it works

--- src/main/java/com/hse/units/repositories/UserRepository.java
+++ src/main/java/com/hse/units/repositories/UserRepository.java
@@ -1,9 +1,9 @@
 package com.hse.units.repositories;
 
 import com.hse.units.entities.User;
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository /*extends CrudRepository<User, Long>*/ {
 }
