da84f456131caf266605c19a730f6743a8cf5b4b
Lily <lilyreber@mail.ru>
2023-05-07 10:22:18 +0300

    mail

--- src/main/java/com/hse/units/services/MailSender.java
+++ src/main/java/com/hse/units/services/MailSender.java
@@ -1,2 +1,27 @@
-package com.hse.units.services;public class MailSenter {
+package com.hse.units.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSender {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    public void send(String emailTo, String subject, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom(username);
+        mailMessage.setTo(emailTo);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+
+        mailSender.send(mailMessage);
+    }
 }
