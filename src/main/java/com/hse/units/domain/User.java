a8aa4e6a057969df73f2ed88d95b58d3483bbf3c
Lily <lilyreber@mail.ru>
2023-05-07 15:13:56 +0300

    account_activation

--- src/main/java/com/hse/units/domain/User.java
+++ src/main/java/com/hse/units/domain/User.java
@@ -31,6 +31,10 @@ public class User implements UserDetails {
 
     private String email;
 
+    private String activationCode;
+
+    protected User() {
+    }
     private String password;
     @Enumerated(EnumType.STRING)
     private final Role role = Role.USER; // TODO: process Role
@@ -61,6 +65,14 @@ public class User implements UserDetails {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
     }
 
     @Override
