3a35fa9f8a1df31b4a0af8b46e75ee2201fcebc7
Lily <lilyreber@mail.ru>
2023-05-24 18:55:28 +0300

    progress

--- src/main/java/com/hse/units/domain/Progress.java
+++ src/main/java/com/hse/units/domain/Progress.java
@@ -1,2 +1,14 @@
-package com.hse.units.domain;public class Progress {
+package com.hse.units.domain;
+
+import java.util.HashMap;
+
+
+
+public class Progress {
+    private static class percentageOfProgress {
+        int easy;
+        int medium;
+        int hard;
+    }
+    HashMap<TaskTag, percentageOfProgress> progress;
 }
