9e38f804eb96a23d5f7da8a3b3fd9d99a0a24730
Jegor Popow <juicedogegor@gmail.com>
2023-06-15 15:46:55 +0300

    small fixes

--- src/main/java/com/hse/units/domain/User.java
+++ src/main/java/com/hse/units/domain/User.java
@@ -77,7 +77,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("user");
+        return List.of(new SimpleGrantedAuthority("user"));
     }
 
     @Override
